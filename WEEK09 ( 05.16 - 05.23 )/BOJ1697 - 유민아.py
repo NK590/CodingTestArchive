7817c1b99541a7a481d65e7a1e81eeae157f44d6
Minah <yucosmosme@gmail.com>
2022-05-21 16:41:38 +0900

    Update BOJ1697 - 유민아.py

--- WEEK09 ( 05.16 - 05.23 )/BOJ1697 - 유민아.py
+++ WEEK09 ( 05.16 - 05.23 )/BOJ1697 - 유민아.py
@@ -33,6 +33,8 @@ def bfs():
     for nx in (x+1, x-1, x*2):
       # print(timeTable[nx])
       # 이동한 위치가 범위 내에 있고 아직 방문하지 않았다면 
+
+      # for문을 한번 돌때 x+1, x-1, x*2의 위치의 이동시간은 (조건에만 해당한다면) 모두 공평하게 +1씩 증가 --> 따라서 먼저 큐에 들어온 수빈의 위치(목적지) 가 그 이후의 for문에서 다른 경로로 접근한 방법보다 이동시간이 짧음 ---> 방문여부 테이블을 따로 만들어서 이미 방문했다면 살펴볼 필요가 없게 알고리즘을 짠다.
       if 0 <= nx <= max_position and not timeTable[nx]:
         # 이동시간 저장
         timeTable[nx] = timeTable[x] + 1
