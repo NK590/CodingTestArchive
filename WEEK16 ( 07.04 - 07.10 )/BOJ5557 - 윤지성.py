a283dbe001cf4f638259bee448d5f84b46437545
Yun Jiseong <73680499+Yunjiseong@users.noreply.github.com>
2022-07-10 21:00:32 +0900

    Create BOJ5557 - 윤지성.py

---
+++ WEEK16 ( 07.04 - 07.10 )/BOJ5557 - 윤지성.py
@@ -0,0 +1,20 @@
+import sys
+
+if __name__ == '__main__':
+    cnt = 0
+    n = int(input())
+    v = list(map(int, sys.stdin.readline().split()))
+    # dp 배열 선언
+    d = [[0 for _ in range(21)] for _ in range(n)]
+
+    d[0][v[0]] = 1
+    for i in range(1, n-1):
+        for j in range(21):
+            if d[i-1][j]: # 이전 인덱스에서 값이 있을 경우
+                if 0<=j+v[i]<=20: # 덧셈
+                    d[i][j+v[i]] += d[i-1][j]
+
+                if 0<=j-v[i]<=20:
+                    d[i][j-v[i]] += d[i-1][j]
+
+    print(d[n-2][v[-1]])
