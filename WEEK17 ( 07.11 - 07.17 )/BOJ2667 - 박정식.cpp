44b995d356a77d16f0a9ba730999bc6781f0886f
StarLightGarden <handock879@gmail.com>
2022-07-17 19:38:30 +0900

    Update BOJ2667 - 박정식.cpp

--- WEEK17 ( 07.11 - 07.17 )/BOJ2667 - 박정식.cpp
+++ WEEK17 ( 07.11 - 07.17 )/BOJ2667 - 박정식.cpp
@@ -13,22 +13,22 @@ vector<int> cntVec;
 int cnt;
 
 // 북서남동
-int dx[] = { 0, 1, 0, -1};
-int dy[] = { 1, 0, -1, 0};
+int dx[] = { 0, 1, 0, -1 };
+int dy[] = { 1, 0, -1, 0 };
 
 int N;
 
 void dfs(int y, int x)
 {
-    for (int i = 0; i < 4; i++)
-    {
-        int ny = y + dy[i];
-        int nx = x + dx[i];
 
-        if (ny >= N || ny < 0 || nx >= N || nx < 0)
+    for (int dir = 0; dir < 4; dir++)
+    {
+        int ny = y + dy[dir];
+        int nx = x + dx[dir];
+        if (ny < 0 || ny >= N || nx < 0 || nx >= N)
             continue;
 
-        if (visit[ny][nx] == 0 && map[ny][nx] == 1)
+        if (map[ny][nx] == 1 && visit[ny][nx] == 0)
         {
             visit[ny][nx] = 1;
             cnt++;
@@ -39,40 +39,40 @@ void dfs(int y, int x)
 
 int main()
 {
-    int res = 0;
     scanf("%d", &N);
 
     // 지도 데이터 입력
-    for (int y = 0; y < N; y++) 
+    for (int y = 0; y < N; y++)
     {
-        for (int x = 0; x < N; x++) 
+        for (int x = 0; x < N; x++)
         {
             scanf("%1d", &map[y][x]);
         }
     }
 
-    for (int y = 0; y < N; y++) 
+
+    for (int y = 0; y < N; y++)
     {
-        for (int x = 0; x < N; x++) 
+        for (int x = 0; x < N; x++)
         {
-            if (map[y][x] == 1 && visit[y][x] == 0) 
+            if (map[y][x] == 1 && visit[y][x] == 0)
             {
                 visit[y][x] = 1;
-                cnt = 1;                        //처음은 시작점 포함하므로 1로 초기화
+                cnt = 1;
                 dfs(y, x);
                 cntVec.push_back(cnt);
-                res++;                          //단지 그룹 1개 탐색 끝남
             }
         }
     }
 
+    int cntVecSize = cntVec.size();
     sort(cntVec.begin(), cntVec.end());
-    cout << res << "\n";
-    for (int i = 0; i < cntVec.size(); i++) 
+
+    printf("%d\n", cntVecSize);
+    for (int index = 0; index < cntVecSize; index++)
     {
-        cout << cntVec[i] << "\n";
+        printf("%d\n", cntVec[index]);
     }
 
-
-	return 0;
+    return 0;
 }
