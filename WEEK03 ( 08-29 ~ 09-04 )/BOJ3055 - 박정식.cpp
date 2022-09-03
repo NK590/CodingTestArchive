5a2cf9a5623c378f65f3dbcf94f8df3b258b56db
StarLightGarden <handock879@gmail.com>
2022-09-03 23:27:27 +0900

    push push

---
+++ WEEK03 ( 08-29 ~ 09-04 )/BOJ3055 - 박정식.cpp
@@ -0,0 +1,125 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 50 + 1
+#define X second
+#define Y first
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+// 상하좌우
+int dx[4] = { 0, 0,-1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
+
+char map[MAX][MAX];
+int R, C;
+
+
+queue < pair<int, int>> sq;
+queue < pair<int, int>> wq;
+
+void MoveWater()
+{
+	int cnt = wq.size();
+	for (int i = 0; i < cnt; i++)
+	{
+		pair<int, int> cur = wq.front();
+		wq.pop();
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+
+			if (ny < 0 || nx < 0 || ny >= R || nx >= C)
+				continue;
+			if (map[ny][nx] != '.')
+				continue;
+
+			map[ny][nx] = '*';
+			wq.push({ny, nx});
+		}
+	}
+}
+
+int MoveS()
+{
+	int time = 0;
+	bool check = false;
+	while (!sq.empty())
+	{
+		time++;
+		MoveWater();
+
+		int cnt = sq.size();
+		for (int i = 0; i < cnt; i++)
+		{
+			pair<int, int> cur = sq.front();
+			sq.pop();
+			for (int dir = 0; dir < 4; dir++)
+			{
+				int nx = cur.X + dx[dir];
+				int ny = cur.Y + dy[dir];
+
+				if (ny < 0 || nx < 0 || ny >= R || nx >= C)
+					continue;
+				if (map[ny][nx] == 'D')
+				{
+					check = true;
+					break;
+				}
+				if (map[ny][nx] != '.')
+					continue;
+
+				map[ny][nx] = 'S';
+				sq.push({ny, nx});
+			}
+
+		}
+		if (check == true)
+			break;
+
+	}
+	if (check == false)
+		time = -1;
+	return time;
+}
+
+void Print()
+{
+	for (int y = 0; y < R; y++)
+	{
+		for (int x = 0; x < C; x++)
+			cout << map[y][x];
+
+		cout << endl;
+	}
+}
+
+int main()
+{
+	cin >> R >> C;
+
+	for (int y = 0; y < R; y++)
+	{
+		for (int x = 0; x < C; x++)
+		{
+			cin >> map[y][x];
+
+			if (map[y][x] == 'S')
+				sq.push({ y, x });
+			else if (map[y][x] == '*')
+				wq.push({y, x});
+		}
+	}	
+	// * : 물, S 고슴도치 위치, 비버의 굴 D, 돌 X
+
+	
+	//Print();
+	int time = MoveS();
+	if (time == -1)
+		cout << "KAKTUS" << endl;
+	else
+		cout << time << endl;
+
+	return 0;
+}
---
+++ WEEK03 ( 08-29 ~ 09-04 )/BOJ5427 - 박정식.cpp
@@ -0,0 +1,146 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MAX 1000 + 1
+#define X second
+#define Y first
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+// »óÇÏÁÂ¿ì
+int dx[4] = { 0, 0,-1, 1 };
+int dy[4] = { 1, -1, 0, 0 };
+
+char map[MAX][MAX];
+int w, h;
+
+
+queue < pair<int, int>> sq;
+queue < pair<int, int>> fq;
+
+void MoveFire()
+{
+	int cnt = fq.size();
+	for (int i = 0; i < cnt; i++)
+	{
+		pair<int, int> cur = fq.front();
+		fq.pop();
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.X + dx[dir];
+			int ny = cur.Y + dy[dir];
+
+			if (ny < 0 || nx < 0 || ny >= h || nx >= w)
+				continue;
+			if (map[ny][nx] != '.')
+				continue;
+
+			map[ny][nx] = '*';
+			fq.push({ ny, nx });
+		}
+	}
+}
+
+int MoveS()
+{
+	int time = 0;
+	bool check = false;
+	while (!sq.empty())
+	{
+		time++;
+		MoveFire();
+
+		int cnt = sq.size();
+		for (int i = 0; i < cnt; i++)
+		{
+			pair<int, int> cur = sq.front();
+			sq.pop();
+			for (int dir = 0; dir < 4; dir++)
+			{
+				int nx = cur.X + dx[dir];
+				int ny = cur.Y + dy[dir];
+
+				if (ny < 0 || nx < 0 || ny >= h || nx >= w)
+				{
+					check = true;
+					break;
+				}
+				if (map[ny][nx] != '.')
+					continue;
+
+				map[ny][nx] = '@';
+				sq.push({ ny, nx });
+			}
+
+		}
+		if (check == true)
+			break;
+
+	}
+	if (check == false)
+		time = -1;
+	return time;
+}
+
+void Print()
+{
+	for (int y = 0; y < h; y++)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			cout << map[y][x];
+		}
+		cout << endl;
+	}
+}
+
+void Reset(int w, int h)
+{
+	for (int y = 0; y < h; y++)
+	{
+		for (int x = 0; x < w; x++)
+		{
+			map[y][x] = '.';
+		}
+	}
+	while (!sq.empty())
+		sq.pop();
+	while (!fq.empty())
+		fq.pop();
+}
+
+int main()
+{
+	int t;
+	cin >> t;	
+
+	for (int i = 0; i < t; i++)
+	{
+		cin >> w >> h;
+		for (int y = 0; y < h; y++)
+		{
+			for (int x = 0; x < w; x++)
+			{
+				cin >> map[y][x];
+
+				if (map[y][x] == '@')
+					sq.push({ y, x });
+				else if (map[y][x] == '*')
+					fq.push({ y, x });
+			}
+		}
+		int time = MoveS();
+		if (time == -1)
+			cout << "IMPOSSIBLE" << endl;
+		else
+			cout << time << endl;
+
+		Reset(w, h);
+		//Print();
+	}
+
+
+
+
+	return 0;
+}
