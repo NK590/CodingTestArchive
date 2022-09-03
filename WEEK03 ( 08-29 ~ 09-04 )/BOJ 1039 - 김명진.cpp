69d8cc5d4904a282f011005dc3942b82dd15ea86
thinkingjeen <111105992+thinkingjeen@users.noreply.github.com>
2022-09-04 03:34:22 +0900

    a

---
+++ WEEK03 ( 08-29 ~ 09-04 )/BOJ 1039 - 김명진.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+#include <queue>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+string S;
+int K, M;
+vector<int> Answer;
+
+int Calcul(string s)
+{
+	int Sum = 0;
+	for (int i = 0; i < s.length(); i++)
+	{
+		Sum *= 10;
+		Sum += (s[i] - '0');
+	}
+	return Sum;
+}
+
+void BFS(string S) 
+{
+    queue<string> Q;
+    Q.push(S);
+    // 바꾼 횟수
+    int cnt = 0;
+    // 찾은 수 중 최대
+    int ans = 0;
+
+    while (!Q.empty() && cnt < K) {
+        int Qs = Q.size();
+        // 똑같은 수 바꾸기 반복 방지 (메모리 초과 방지)
+        set<string> Visit;
+
+        // cnt=i 일때 i번 바뀐수들이 Q에 저장되어 있다.
+        // 모든 i번 바뀐 수들을 한번씩 더 바꿔본다.
+        for (int q = 0; q < Qs; q++) {
+            string now = Q.front();
+            Q.pop();
+            // 숫자 바꿔보기
+            for (int i = 0; i < M - 1; i++) {
+                for (int j = i + 1; j < M; j++) {
+                    // 0 으로 숫자가 시작하면 안된다.
+                    if (i == 0 && now[j] == '0') continue;
+
+                    swap(now[i], now[j]);
+                    if (!Visit.count(now)) {
+                        // K번 바꾼경우 최대 수 찾기
+                        if (cnt == K - 1)
+                            ans = max(ans, Calcul(now));
+
+                        Q.push(now);
+                        Visit.insert(now);
+                    }
+                    swap(now[i], now[j]);
+                }
+            }
+        }
+        cnt++;
+    }
+    if (cnt != K) cout << -1 << endl;
+    else cout << ans << endl;
+}
+
+int main()
+{
+    cin >> S >> K;
+    M = S.length();
+    // 주어진 수가 1자리 수일때 or 2자리이고 뒷자리 0 일때
+    if (M == 1 || (M == 2 && Calcul(S) % 10 == 0))
+        cout << "-1" << endl;
+    else
+        BFS(S);
+
+    return 0;
+}
---
+++ WEEK03 ( 08-29 ~ 09-04 )/BOJ 7576 -  김명진.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <queue>
+
+using namespace std;
+#define MAX 1001
+
+int N, M;
+int days = -1;
+bool flag = false;
+int tomato[MAX][MAX];
+int visited[MAX][MAX];
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,1,0,-1 };
+queue<pair<int, int>> q;
+
+int main() {
+    cin >> M >> N;
+
+    for (int i = 0;i < N;i++) {
+        for (int j = 0;j < M;j++) {
+            cin >> tomato[i][j];
+            visited[i][j] = -1;
+
+            if (tomato[i][j] == 1) {
+                q.push({ i,j });
+                visited[i][j]++;
+            }
+        }
+    }
+
+    while (!q.empty()) {
+        int a = q.front().first;
+        int b = q.front().second;
+        q.pop();
+        for (int i = 0;i < 4;i++) {
+            int na = a + dx[i];
+            int nb = b + dy[i];
+            if (na >= 0 && nb >= 0 && N > na && M > nb && visited[na][nb] == -1 && tomato[na][nb] == 0) {
+                q.push({ na,nb });
+                visited[na][nb] = visited[a][b] + 1;
+            }
+        }
+    }
+
+    for (int i = 0;i < N;i++) {
+        for (int j = 0;j < M;j++) {
+            if (days <= visited[i][j]) {
+                days = visited[i][j];
+            }
+            if (tomato[i][j] == 0 && visited[i][j] == -1) {
+                days = -1;
+                flag = true;
+                break;
+            }
+        }
+        if (flag)
+            break;
+    }
+    cout << days;
+}
