ee6de2dca2af8a9e57a141a7e12faf29f60fd201
thinkingjeen <111105992+thinkingjeen@users.noreply.github.com>
2022-10-02 00:44:56 +0900

    0

---
+++ WEEK07 ( 09-26 ~ 10-02 )/BOJ 1759 - 김명진.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <list>
+
+using namespace std;
+
+int L, C;
+list<char> lInput;
+char sortInput[15];
+int password[15];
+
+void Password(int n, int usableIdx, int cnt)
+{
+	int temp;
+	if (n == L)
+	{
+		if (password[L - 1] >= L - 1)
+		{
+			if (cnt >= 1 && L - cnt >= 2)
+			{
+				for (int i = 0; i < L; i++)
+				{
+					temp = password[i];
+					cout << sortInput[temp];
+				}
+				cout << "\n";
+			}
+		}
+	}
+	else
+	{
+		for (int i = usableIdx; i < C; i++)
+		{
+			password[n] = i;
+			if (sortInput[i] == 'a' || sortInput[i] == 'e' || sortInput[i] == 'i' || sortInput[i] == 'o' || sortInput[i] == 'u')
+			{
+				Password(n + 1, i + 1, cnt+1);
+			}
+			else
+			{
+				Password(n + 1, i + 1, cnt);
+			}
+		}
+	}
+}
+
+int main()
+{
+	cin >> L >> C;
+	for (int i = 0; i < C; i++)
+	{
+		char temp;
+		cin >> temp;
+		lInput.push_back(temp);
+	}
+	lInput.sort();
+	for (int i = 0; i < C; i++)
+	{
+		char temp;
+		temp = lInput.front();
+		sortInput[i] = temp;
+		lInput.pop_front();
+	}
+	Password(0, 0, 0);
+}
---
+++ WEEK07 ( 09-26 ~ 10-02 )/BOJ 1987 - 김명진.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int R, C;
+int board[20][20];
+string inputTemp;
+bool visited[91];
+int dirX[] = { 0,0,-1,1 };
+int dirY[] = { -1,1,0,0 };
+int ans;
+
+void Reset()
+{
+	for (int i = 65; i < 91; i++)
+	{
+		visited[i] = false;
+	}
+}
+
+void Alphabet(int Y, int X, int cnt)
+{
+	visited[board[Y][X]] = true;
+	for (int i = 0; i < 4; i++)
+	{
+		if (Y + dirY[i] >= 0 && Y + dirY[i] <= R-1 && X + dirX[i] >= 0 && X + dirX[i] <= C-1)
+		{
+			if (!visited[board[Y + dirY[i]][X + dirX[i]]])
+			{
+				Alphabet(Y + dirY[i], X + dirX[i], cnt + 1);
+			}
+		}
+	}
+	visited[board[Y][X]] = false;
+
+	if (cnt > ans)
+	{
+		ans = cnt;
+	}
+}
+
+int main()
+{
+	cin >> R >> C;
+	for (int i = 0; i < R; i++)
+	{
+		cin >> inputTemp;
+		for (int j = 0; j < C; j++)
+		{
+			board[i][j] = (int)inputTemp[j];
+		}
+	}
+	Reset();
+	Alphabet(0, 0, 1);
+	cout << ans;
+}
