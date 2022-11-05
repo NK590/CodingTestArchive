061d41868cfa3cc324e1f3a5a8acfdfc8b45adfb
thinkingjeen <audwls607@gmail.com>
2022-11-05 19:12:32 +0900

    0
    0

---
+++ WEEK10 ( 10-31 ~ 11-06 )/BOJ 16113 - 김명진.cpp
@@ -0,0 +1,115 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+bool board[5][20000];
+
+int main()
+{
+	int n;
+	string signal;
+	vector<int> result;
+
+	cin >> n >> signal;
+
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < signal.length() / 5; ++j)
+		{
+			if (signal[(i * signal.length() / 5) + j] == '#')
+			{
+				board[i][j] = 1;
+			}
+		}
+	}
+
+	string check;
+	for (int j = 0; j < signal.length() / 5; ++j)
+	{
+		check.clear();
+		if (board[0][j] == 1)
+		{
+			if ( j == 19999 ||(board[0][j + 1] == 0 && board[1][j + 1] == 0 && board[2][j + 1] == 0 && board[3][j + 1] == 0 && board[4][j + 1] == 0))
+			{
+				result.push_back(1);
+				continue;
+			}
+			else
+			{
+				for (int k = 0; k < 3; ++k)
+				{
+					for (int l = 0; l < 5; ++l)
+					{
+						if (board[0 + l][j + k] == 1)
+						{
+							check += "1";
+						}
+						else
+						{
+							check += "0";
+						}
+					}
+				}
+				if (check == "111111000111111")
+				{
+					result.push_back(0);
+					j += 2;
+					continue;
+				}
+				else if (check == "101111010111101")
+				{
+					result.push_back(2);
+					j += 2;
+					continue;
+				}
+				else if (check == "101011010111111")
+				{
+					result.push_back(3);
+					j += 2;
+					continue;
+				}
+				else if (check == "111000010011111")
+				{
+					result.push_back(4);
+					j += 2;
+					continue;
+				}
+				else if (check == "111011010110111")
+				{
+					result.push_back(5);
+					j += 2;
+					continue;
+				}
+				else if (check == "111111010110111")
+				{
+					result.push_back(6);
+					j += 2;
+					continue;
+				}
+				else if (check == "100001000011111")
+				{
+					result.push_back(7);
+					j += 2;
+					continue;
+				}
+				else if (check == "111111010111111")
+				{
+					result.push_back(8);
+					j += 2;
+					continue;
+				}
+				else if (check == "111011010111111")
+				{
+					result.push_back(9);
+					j += 2;
+					continue;
+				}
+			}
+		}
+	}
+	for (int i = 0; i < result.size(); ++i)
+	{
+		cout << result[i];
+	}
+}
---
+++ WEEK10 ( 10-31 ~ 11-06 )/BOJ 2290 - 김명진.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void PrintEmpty(int s)
+{
+	for (int i = 0; i < s + 2; ++i)
+	{
+		cout << " ";
+	}
+}
+void PrintHorizon(int s) 
+{
+    cout << " ";
+    for (int i = 0; i < s; i++) 
+    { 
+        cout << "-";
+    }
+    cout << " ";;
+}
+
+void PrintVertRight(int s) 
+{
+    for (int i = 0; i < s + 1; i++) 
+    { 
+        cout << " "; 
+    }
+    cout << "|";
+}
+void PrintVertLeft(int s) 
+{
+    cout << "|";
+    for (int i = 0; i < s + 1; i++) 
+    { 
+        cout << " "; 
+    }
+}
+void PrintVertBoth(int s) 
+{
+    cout << "|";
+    for (int i = 0; i < s; i++) 
+    { 
+        cout << " "; 
+    }
+    cout << "|";
+}
+
+
+void PrintOne(int s, int r) 
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintEmpty(s);
+    else PrintVertRight(s);
+}
+void PrintTwo(int s, int r)
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintHorizon(s);
+    else if (2 <= r && r <= s + 1) PrintVertRight(s);
+    else PrintVertLeft(s);
+}
+void PrintThree(int s, int r) 
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintHorizon(s);
+    else PrintVertRight(s);
+}
+void PrintFour(int s, int r) 
+{
+    if (r == 1 || r == 2 * s + 3) PrintEmpty(s);
+    else if (r == s + 2) PrintHorizon(s);
+    else if (2 <= r && r <= s + 1) PrintVertBoth(s);
+    else PrintVertRight(s);
+}
+void PrintFive(int s, int r) 
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintHorizon(s);
+    else if (2 <= r && r <= s + 1) PrintVertLeft(s);
+    else PrintVertRight(s);
+}
+void PrintSix(int s, int r) 
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintHorizon(s);
+    else if (2 <= r && r <= s + 1) PrintVertLeft(s);
+    else PrintVertBoth(s);
+}
+void PrintSeven(int s, int r) 
+{
+    if (r == 1) PrintHorizon(s);
+    else if (r == s + 2 || r == 2 * s + 3) PrintEmpty(s);
+    else PrintVertRight(s);
+}
+void PrintEight(int s, int r) 
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintHorizon(s);
+    else PrintVertBoth(s);
+}
+void PrintNine(int s, int r) 
+{
+    if (r == 1 || r == s + 2 || r == 2 * s + 3) PrintHorizon(s);
+    else if (2 <= r && r <= s + 1) PrintVertBoth(s);
+    else PrintVertRight(s);
+}
+void PrintZero(int s, int r) 
+{
+    if (r == 1 || r == 2 * s + 3) PrintHorizon(s);
+    else if (r == s + 2) PrintEmpty(s);
+    else PrintVertBoth(s);
+}
+
+int main()
+{
+	int s;
+	string n;
+	cin >> s >> n;
+
+    for (int r = 1; r <= 2 * s + 3; ++r)
+    {
+        for (int i = 0; i < n.length();++i)
+        {
+            if (n[i] == '1') PrintOne(s, r); 
+            else if (n[i] == '2')  PrintTwo(s, r); 
+            else if (n[i] == '3')  PrintThree(s, r); 
+            else if (n[i] == '4')  PrintFour(s, r); 
+            else if (n[i] == '5')  PrintFive(s, r);
+            else if (n[i] == '6')  PrintSix(s, r);
+            else if (n[i] == '7')  PrintSeven(s, r); 
+            else if (n[i] == '8')  PrintEight(s, r); 
+            else if (n[i] == '9')  PrintNine(s, r); 
+            else if (n[i] == '0')  PrintZero(s, r);
+
+            cout << " ";
+        }
+        cout << "\n";
+    }
+}
