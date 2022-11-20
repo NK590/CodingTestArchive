b09e8cb9a52d2aaafacc2c9d573f4da18aef311f
thinkingjeen <audwls607@gmail.com>
2022-11-20 17:37:47 +0900

    0
    0

---
+++ WEEK11 ( 11-07 ~ 11-13 )/BOJ 1194 - 김명진.cpp
@@ -0,0 +1,91 @@
+﻿#include<iostream>
+#include<queue>
+
+using namespace std;
+
+int N, M;
+char map[50][50];
+bool visited[50][50][1 << 6];
+
+pair<int, int> start;
+
+int dx[] = { 0, 0, -1, 1 };
+int dy[] = { 1, -1, 0, 0 };
+
+int BFS(int a, int b)
+{
+    queue<pair<pair<int, int>, pair<int, int>>> Q;
+    Q.push(make_pair(make_pair(a, b), make_pair(0, 0)));
+    visited[a][b][0] = true;
+
+    while (Q.empty() == 0)
+    {
+        int x = Q.front().first.first;
+        int y = Q.front().first.second;
+        int Cnt = Q.front().second.first;
+        int keys = Q.front().second.second;
+        Q.pop();
+
+        if (map[x][y] == '1')
+        {
+            return Cnt;
+        }
+
+        for (int i = 0; i < 4; i++)
+        {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M)
+            {
+                if (visited[nx][ny][keys] == false)
+                {
+                    if (map[nx][ny] == '.' || map[nx][ny] == '1')
+                    {
+                        visited[nx][ny][keys] = true;
+                        Q.push(make_pair(make_pair(nx, ny), make_pair(Cnt + 1, keys)));
+                    }
+                    else if ('a' <= map[nx][ny] && map[nx][ny] <= 'f')
+                    {
+                        int Tmp_Key = keys | (1 << (int(map[nx][ny] - 'a')));
+                        visited[nx][ny][Tmp_Key] = true;
+                        Q.push(make_pair(make_pair(nx, ny), make_pair(Cnt + 1, Tmp_Key)));
+                    }
+                    else if ('A' <= map[nx][ny] && map[nx][ny] <= 'F')
+                    {
+                        if ((keys & (1 << (int(map[nx][ny]) - 'A'))) != 0)
+                        {
+                            visited[nx][ny][keys] = true;
+                            Q.push(make_pair(make_pair(nx, ny), make_pair(Cnt + 1, keys)));
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return -1;
+}
+
+int main(void)
+{
+    cin >> N >> M;
+    for (int i = 0; i < N; i++)
+    {
+        string Inp; cin >> Inp;
+        for (int j = 0; j < M; j++)
+        {
+            map[i][j] = Inp[j];
+            if (map[i][j] == '0')
+            {
+                start.first = i;
+                start.second = j;
+                map[i][j] = '.';
+            }
+        }
+    }
+
+    int result = BFS(start.first, start.second);
+    cout << result << "\n";
+
+    return 0;
+}
---
+++ WEEK11 ( 11-07 ~ 11-13 )/BOJ11559 - 김명진.cpp
@@ -0,0 +1,141 @@
+﻿#include<iostream>
+#include<queue>
+
+using namespace std;
+
+char board[12][6];
+bool visit[12][6];
+bool isBoom[12][6];
+int continuity = 0;
+int dx[4] = { 0,0,-1,1 };
+int dy[4] = { 1,-1,0,0 };
+
+void Gravity() 
+{ 
+	for (int i = 11; i >= 0; i--) 
+	{
+		for (int j = 5; j >= 0; j--) 
+		{
+			int dist = 0;
+			if (board[i][j] == '.') 
+			{
+				for (int k = i; k >= 0; k--)
+				{
+					if (board[k][j] == '.')
+					{
+						dist++;
+					}
+					else 
+					{
+						while (dist--) 
+						{
+							board[k + 1][j] = board[k][j];
+							board[k][j] = '.';
+							k++;
+						}
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+void explode()
+{
+	for (int i = 0; i < 12; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			if (isBoom[i][j] == true) 
+			{
+				board[i][j] = '.';
+			}
+		}
+	}
+}
+
+int bfs(int y, int x) 
+{
+	queue<pair<int, int>> q;
+	queue<pair<int, int>> candidate;
+	q.push({y,x});
+	candidate.push({ y,x });
+	visit[y][x] = true;
+	char color = board[y][x];
+	int cnt = 1;
+	while (!q.empty()) 
+	{
+		y = q.front().first;
+		x = q.front().second;
+		q.pop();
+		for (int i = 0; i < 4; i++) 
+		{
+			int ny = y + dy[i];
+			int nx = x + dx[i];
+			char new_color = board[ny][nx];
+			if (0 <= nx && nx < 6 && 0 <= ny && ny < 12 && !visit[ny][nx] && new_color == color) 
+			{
+				visit[ny][nx] = true;
+				q.push({ ny,nx });
+				candidate.push({ ny,nx });
+				cnt++;
+			}
+		}
+	}
+
+	if (candidate.size() >= 4)
+	{
+		int repeat = candidate.size();
+		for (int i = 0; i < repeat; ++i)
+		{
+			isBoom[candidate.front().first][candidate.front().second] = true;
+			candidate.pop();
+		}
+	}
+	return cnt;
+}
+
+int main() 
+{
+	for (int i = 0; i < 12; i++) 
+	{
+		for (int j = 0; j < 6; j++) 
+		{
+			cin >> board[i][j];
+		}
+	}
+	bool is_explode = true;
+	while (is_explode)
+	{
+		int puyoSize = 0;
+		is_explode = false;
+		for (int i = 0; i < 12; i++) 
+		{
+			for (int j = 0; j < 6; j++) 
+			{
+				if (board[i][j] != '.' && !visit[i][j])
+				{
+					puyoSize = bfs(i, j);
+					if (puyoSize >= 4)
+					{ 
+						explode();
+						is_explode = true;
+					}
+				}
+			}
+		}
+		if (is_explode) continuity++;
+
+		for (int i = 0; i < 12; i++)
+		{
+			for (int j = 0; j < 6; j++)
+			{
+				visit[i][j] = false;
+				isBoom[i][j] = false;
+			}
+		}
+		Gravity();
+	}
+	cout << continuity;
+}
