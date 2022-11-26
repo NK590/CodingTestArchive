43b77054d0b78c6ac16f86c34c3c2d69beaa0cd2
thinkingjeen <audwls607@gmail.com>
2022-11-26 23:05:09 +0900

    0
    0

---
+++ WEEK13 ( 11-21 ~ 11-27 )/BOJ 6987 - 김명진 .cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+const int homeT[] = { 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 3, 3, 4 };
+const int awayT[] = { 1, 2, 3, 4, 5, 2, 3, 4, 5, 3, 4, 5, 4, 5, 5 };
+
+int answer[4], caseBoard[6][3], result[6][3];
+
+void DFS(int caseNum, int matchNum) {
+    if (matchNum == 15) {
+        if (answer[caseNum]) return;
+
+        for (int r = 0; r < 6; r++) {
+            for (int c = 0; c < 3; c++) {
+                if (caseBoard[r][c] != result[r][c])
+                    return;
+            }
+        }
+
+        answer[caseNum] = 1;
+        return;
+    }
+
+    int t1 = homeT[matchNum];
+    int t2 = awayT[matchNum];
+
+    result[t1][0]++; result[t2][2]++;
+    DFS(caseNum, matchNum + 1);
+    result[t1][0]--; result[t2][2]--;
+
+    result[t1][1]++; result[t2][1]++;
+    DFS(caseNum, matchNum + 1);
+    result[t1][1]--; result[t2][1]--;
+
+    result[t1][2]++; result[t2][0]++;
+    DFS(caseNum, matchNum + 1);
+    result[t1][2]--; result[t2][0]--;
+}
+
+
+int main() {
+
+    for (int i = 0; i < 4; ++i) {
+        for (int r = 0; r < 6; ++r) {
+            for (int c = 0; c < 3; c++) {
+                cin >> caseBoard[r][c];
+            }
+        }
+        DFS(i, 0);
+    }
+
+    for (int i = 0; i < 4; i++)
+        cout << answer[i] << ' ';
+    cout << "\n";
+}
---
+++ WEEK13 ( 11-21 ~ 11-27 )/BOJ 8911 - 김명진.cpp
@@ -0,0 +1,133 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+enum class eDirection
+{
+	N, W, S, E
+};
+
+eDirection dir = eDirection::N;
+
+int top = 0;
+int rightF = 0;
+int bottom = 0;
+int leftF = 0;
+
+int testNum;
+string input;
+
+int Calculate(string input)
+{
+	pair<int, int> cur = { 0, 0 };
+
+	for (int i = 0; i < input.size(); ++i)
+	{
+		if (input[i] == 'F')
+		{
+			switch (dir)
+			{
+			case eDirection::N:
+				cur.second += 1;
+				if (cur.second > top)
+				{
+					top = cur.second;
+				}
+				break;
+			case eDirection::W:
+				cur.first -= 1;
+				if (cur.first < leftF)
+				{
+					leftF = cur.first;
+				}
+				break;
+			case eDirection::S:
+				cur.second -= 1;
+				if (cur.second < bottom)
+				{
+					bottom = cur.second;
+				}
+				break;
+			case eDirection::E:
+				cur.first += 1;
+				if (cur.first > rightF)
+				{
+					rightF = cur.first;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
+		else if (input[i] == 'B')
+		{
+			switch (dir)
+			{
+			case eDirection::N:
+				cur.second -= 1;
+				if (cur.second < bottom)
+				{
+					bottom = cur.second;
+				}
+				break;
+			case eDirection::W:
+				cur.first += 1;
+				if (cur.first > rightF)
+				{
+					rightF = cur.first;
+				}
+				break;
+			case eDirection::S:
+				cur.second += 1;
+				if (cur.second > top)
+				{
+					top = cur.second;
+				}
+				break;
+			case eDirection::E:
+				cur.first -= 1;
+				if (cur.first < leftF)
+				{
+					leftF = cur.first;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+
+		else if (input[i] == 'L')
+		{
+			dir = (eDirection)(((int)dir + 1) % 4);
+		}
+		else if (input[i] == 'R')
+		{
+			dir = (eDirection)(((int)dir + 3) % 4);
+		}
+	}
+
+	return ((top - bottom) * (rightF - leftF));
+}
+
+
+int main()
+{
+	cin >> testNum;
+	vector<int> result;
+	for (int i = 0; i < testNum; ++i)
+	{
+		cin >> input;
+		result.push_back(Calculate(input));
+		top = 0;
+		bottom = 0;
+		rightF = 0;
+		leftF = 0;
+	}
+	
+	for (int i = 0; i < testNum; ++i)
+	{
+		cout << result[i] << "\n";
+	}
+}
