096eefe233e35304d6592762693780ddeae84925
thinkingjeen <audwls607@gmail.com>
2022-12-11 19:55:52 +0900

    0
    0

---
+++ WEEK15 ( 12-05 ~ 12-11 )/BOJ 14891 - 김명진.cpp
@@ -0,0 +1,188 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+string gears[4];
+int inputGear[100];
+int inputDir[100];
+
+void RotateGear(int gearNum, int dir)
+{
+	//cout << "rotateGear =>" << gearNum + 1 << "\t" << "dir =>" << dir << "\n";
+	string temp = "00000000";
+
+	if (dir < 0)
+	{
+		for (int i = 0; i < 8; ++i)
+		{
+			temp[i] = gears[gearNum][(i + 1) % 8];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < 8; ++i)
+		{
+			temp[i] = gears[gearNum][(i + 7) % 8];
+		}
+	}
+
+	gears[gearNum] = temp;
+}
+
+void ControlGear(int gearNum, int dir)
+{
+	//cout << "controlGear =>" << gearNum + 1 << "\t" <<"dir =>" << dir << "\n";
+
+	bool between[3];
+	for (int i = 0; i < 3; ++i)
+	{
+		if (gears[i][2] != gears[i + 1][6])
+		{
+			between[i] = true;
+		}
+		else
+		{
+			between[i] = false;
+		}
+	}
+
+	int curDir = dir;
+
+	switch (gearNum)
+	{
+	case 0:
+			if (between[0])
+			{
+				curDir *= -1;
+				RotateGear(1, curDir);
+				if (between[1])
+				{
+					curDir *= -1;
+					RotateGear(2, curDir);
+					if (between[2])
+					{
+						curDir *= -1;
+						RotateGear(3, curDir);
+					}
+				}
+			}
+		break;
+	case 1:
+		curDir *= -1;
+		if (between[0])
+		{
+			RotateGear(0, curDir);
+		}
+		if (between[1])
+		{
+			RotateGear(2, curDir);
+			if (between[2])
+			{
+				curDir *= -1;
+				RotateGear(3, curDir);
+			}
+		}
+		break;
+	case 2:
+		curDir *= -1;
+		if (between[2])
+		{
+			RotateGear(3, curDir);
+		}
+		if (between[1])
+		{
+			RotateGear(1, curDir);
+			if (between[0])
+			{
+				curDir *= -1;
+				RotateGear(0, curDir);
+			}
+		}
+		break;
+	case 3:
+			if (between[2])
+			{
+				curDir *= -1;
+				RotateGear(2, curDir);
+				if (between[1])
+				{
+					curDir *= -1;
+					RotateGear(1, curDir);
+					if (between[0])
+					{
+						curDir *= -1;
+						RotateGear(0, curDir);
+					}
+				}
+			}
+		break;
+	}
+
+	RotateGear(gearNum, dir);
+}
+
+float CalculateScore()
+{
+	float score = 0;
+	for (int i = 0; i < 4; ++i)
+	{
+		if (gears[i][0] == '1')
+		{
+			score += pow(2, i);
+		}
+	}
+	return score;
+}
+
+void DebugGear()
+{
+	cout << "==================================" << "\n";
+	for (int i = 0; i < 4; ++i)
+	{
+		cout << gears[i] << "\n";
+	}
+	cout << "==================================" << "\n";
+
+	for (int i = 0; i < 3; ++i)
+	{
+		if (gears[i][2] != gears[i + 1][6])
+		{
+			cout << "between" << i << "===========O============" << "\n";
+		}
+		else
+		{
+			cout << "between" << i << "===========X============" << "\n";
+		}
+	}
+
+}
+
+
+int main()
+{
+	for (int i = 0; i < 4; ++i)
+	{
+		cin >> gears[i];
+	}
+
+	int rotateNum;
+
+	cin >> rotateNum;
+
+	for (int i = 0; i < rotateNum; ++i)
+	{
+		cin >> inputGear[i];
+		cin >> inputDir[i];
+	}
+
+	for (int i = 0; i < rotateNum; ++i)
+	{
+		//DebugGear();
+		ControlGear(inputGear[i]-1, inputDir[i]);
+	}
+
+	//DebugGear();
+
+	cout << round(CalculateScore());
+}
---
+++ WEEK15 ( 12-05 ~ 12-11 )/BOJ 18111 - 김명진.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int ground[500][500];
+
+int main()
+{
+	int row, column, ownBlocks;
+	cin >> row >> column >> ownBlocks;
+
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < column; ++j)
+		{
+			cin >> ground[i][j];
+		}
+	}
+
+	int bestTime = 2100000000;
+	int bestHeight = 0;;
+	for (int k = 0; k <= 256; ++k)
+	{
+		int temp = 0;
+		int blocks = ownBlocks;
+		for (int i = 0; i < row; ++i)
+		{
+			for (int j = 0; j < column; ++j)
+			{
+				if (k > ground[i][j])
+				{
+					temp += k - ground[i][j];
+					blocks -= k- ground[i][j];
+				}
+				else if (k < ground[i][j])
+				{
+					temp += 2 * (ground[i][j] - k);
+					blocks += ground[i][j] - k;
+				}
+				else continue;
+			}
+		}
+		if (blocks >= 0)
+		{
+			if (temp <= bestTime)
+			{
+				bestTime = temp;
+				bestHeight = k;
+			}
+		}
+	}
+
+	cout << bestTime << " " << bestHeight;
+}
