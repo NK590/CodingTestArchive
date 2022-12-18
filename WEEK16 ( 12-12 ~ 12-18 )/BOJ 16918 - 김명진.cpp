7659fa8c8b1933aabe4ffbf1d8995846a808f8fc
thinkingjeen <audwls607@gmail.com>
2022-12-18 20:28:54 +0900

    0
    0

---
+++ WEEK16 ( 12-12 ~ 12-18 )/BOJ 16918 - 김명진.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int row;
+int column;
+int second;
+int ground[201][201];
+
+void spendTime()
+{
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            if (ground[i][j] < 3)
+            {
+                ++ground[i][j];
+            }
+            else
+            {
+                int dx[3] = { -1, 0,0 };
+                int dy[3] = { 0, -1,0 };
+                for (int k = 0; k < 3; ++k)
+                {
+                    if (0 <= i + dx[k] && i + dx[k] <= row && 0 <= j + dy[k] && j + dy[k] <= column)
+                    {
+                        ground[i + dx[k]][j + dy[k]] = 0;
+                    }
+                }
+
+                int dx2[2] = { 0,1 };
+                int dy2[2] = { 1,0 };
+                for (int k = 0; k < 2; ++k)
+                {
+                    if (0 <= i + dx2[k] && i + dx2[k] <= row && 0 <= j + dy2[k] && j + dy2[k] <= column && ground[i + dx2[k]][j + dy2[k]] != 3)
+                    {
+                        ground[i + dx2[k]][j + dy2[k]] = -1;
+                    }
+                }
+            }
+        }
+    }
+}
+
+void printResult()
+{
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            if (ground[i][j] == 0)
+            {
+                cout << '.';
+            }
+            else
+            {
+                cout << 'O';
+            }
+        }
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    cin >> row >> column >> second;
+
+    string temp;
+    for (int i = 0; i < row; ++i)
+    {
+        cin >> temp;
+        for (int j = 0; j < column; ++j)
+        {
+            if (temp[j] == '.')
+            {
+                ground[i][j] = 0;
+            }
+            else
+            {
+                ground[i][j] = 2;
+            }
+        }
+        temp.clear();
+    }
+
+    for (int i = 0; i < second-1; ++i)
+    {
+        spendTime();
+    }
+    printResult();
+}
---
+++ WEEK16 ( 12-12 ~ 12-18 )/BOJ 3190 - 김명진.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int arr[102][102];
+int dx[4] = { 0,1,0,-1 };
+int dy[4] = { 1,0,-1,0 };
+
+int main() 
+{
+    int N, K;
+    cin >> N >> K;
+
+    for (int i = 0; i < K; i++) 
+    {
+        int x, y;
+        cin >> x >> y;
+        arr[x][y] = 4;
+    }
+
+    int L;
+    cin >> L;
+    vector<pair<int, char>> turnInfo;
+
+    for (int i = 0; i < L; i++) 
+    {
+        int sec;
+        char dir;
+        cin >> sec >> dir;
+        turnInfo.push_back({ sec, dir });
+    }
+
+
+    int time = 0;
+    int dir = 0;
+    int index = 0;
+    pair<int, int> head = { 1,1 };
+    queue<pair<int, int>> snake;
+    arr[1][1] = 2;
+    snake.push(head);
+
+    while (1) 
+    {
+        time++;
+
+        head.first += dx[dir];
+        head.second += dy[dir];
+        int nx = head.first;
+        int ny = head.second;
+
+        if (nx > N || ny > N || nx < 1 || ny < 1 || arr[nx][ny] == 2) 
+        {
+            cout << time;
+            return 0;
+        }
+        else if (arr[nx][ny] == 0) 
+        {
+            pair<int, int> cur = snake.front();
+            snake.pop();
+            arr[cur.first][cur.second] = 0;
+        }
+
+        
+        arr[nx][ny] = 2;
+        snake.push({ nx, ny });
+
+        if (time == turnInfo[index].first) 
+        {
+            if (turnInfo[index].second == 'D') 
+            {
+                dir = (dir + 1) % 4;
+                index++;
+            }
+            else if (turnInfo[index].second == 'L') 
+            {
+                dir = (dir + 3) % 4;
+                index++;
+            }
+        }
+    }
+}
