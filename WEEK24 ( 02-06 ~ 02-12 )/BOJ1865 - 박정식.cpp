cd20e331d32f9b7a501b5b961fd2685d4babc48e
StarLightGarden <handock879@gmail.com>
2023-02-12 08:27:34 +0900

    Update BOJ1865 - 박정식.cpp

--- WEEK24 ( 02-06 ~ 02-12 )/BOJ1865 - 박정식.cpp
+++ WEEK24 ( 02-06 ~ 02-12 )/BOJ1865 - 박정식.cpp
@@ -12,20 +12,18 @@ int dist[501];
 vector<pair<int, int>> graph[501];
 
 // 사이클이 있다면 true반환
-bool BellmanFord() 
+bool BellmanFord()
 {
-	//음의 사이클을 갖는지 확인
-	bool cycle = false;
 	// 모든 노드 INF 초기화
 	for (int i = 1; i <= N; i++)
-		dist[i] = INF; 
+		dist[i] = INF;
 
 	// 시작점 0으로 초기화
-	dist[1] = 0; 
+	dist[1] = 0;
 
-	for (int i = 1; i <= N; i++) 
+	for (int i = 1; i <= N; i++)
 	{
-		for (int j = 1; j <= N; j++) 
+		for (int j = 1; j <= N; j++)
 		{
 			// j노드를 기준으로 연결된 노드들 체크
 			for (int k = 0; k < graph[j].size(); k++)
@@ -33,7 +31,7 @@ bool BellmanFord()
 				int next = graph[j][k].second;
 				int cost = graph[j][k].first;
 
-				if (dist[next] > dist[j] + cost) 
+				if (dist[next] > dist[j] + cost)
 				{
 					dist[next] = dist[j] + cost;
 					// N-1까지 확인한 이후에도 갱신이 된다면 음의 사이클이 존재
@@ -52,12 +50,12 @@ void Solution()
 	{
 		cin >> N >> M >> W;
 
+		int from;
+		int to;
+		int distance;
+
 		for (int i = 1; i <= M; i++)
 		{
-			int from;
-			int to;
-			int distance;
-
 			cin >> from >> to >> distance;
 
 			graph[from].push_back(make_pair(distance, to));
@@ -66,10 +64,6 @@ void Solution()
 
 		for (int i = 1; i <= W; i++)
 		{
-			int from;
-			int to;
-			int distance;
-
 			cin >> from >> to >> distance;
 
 			graph[from].push_back(make_pair(-distance, to));
