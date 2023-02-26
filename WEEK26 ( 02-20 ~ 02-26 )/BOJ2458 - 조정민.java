1ee380316073daa11071920ab5970e29e904396c
mnj190 <68101263+mnj190@users.noreply.github.com>
2023-02-26 17:08:32 +0900

    Update and rename Main.java to BOJ2458 - 조정민.java

--- WEEK26 ( 02-20 ~ 02-26 )/BOJ2458 - 조정민.java
+++ WEEK26 ( 02-20 ~ 02-26 )/BOJ2458 - 조정민.java
@@ -1,10 +1,7 @@
-package cordingTest2;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Arrays;
 
 public class Main {
 
