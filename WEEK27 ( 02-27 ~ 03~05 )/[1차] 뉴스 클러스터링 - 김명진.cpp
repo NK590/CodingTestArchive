b3c011a6da787f3ffadc37448dad27075d4ed63c
thinkingjeen <111105992+thinkingjeen@users.noreply.github.com>
2023-03-05 00:23:44 +0900

    0
    0

---
+++ WEEK27 ( 02-27 ~ 03~05 )/[1차] 뉴스 클러스터링 - 김명진.cpp
@@ -0,0 +1,166 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(string str1, string str2)
+{
+	vector<pair<char, char>> temp1;
+	vector<pair<char, char>> temp2;
+	vector<pair<int, pair<char, char>>> count1;
+	vector<pair<int, pair<char, char>>> count2;
+
+	int interSection = 0;
+	int unionSection = 0;
+
+	int answer = 0;
+
+	for (int i = 0; i < str1.size()-1; ++i)
+	{
+		short check = 0;
+		for (int j = 0; j < 2; ++j)
+		{
+			if ('A' <= str1[i + j] && str1[i + j] <= 'Z')
+			{
+				if (j == 0)
+				{
+					check += 1;
+				}
+				else
+				{
+					check += 2;
+				}
+				continue;
+			}
+			else if ('a' <= str1[i + j] && str1[i + j] <= 'z')
+			{
+				continue;
+			}
+			else
+			{
+				check = -1;
+				break;
+			}
+		}
+		if (check == 0)
+		{
+			temp1.push_back(make_pair(str1[i], str1[i + 1]));
+		}
+		else if (check == 1)
+		{
+			temp1.push_back(make_pair(str1[i]+32, str1[i + 1]));
+		}
+		else if (check == 2)
+		{
+			temp1.push_back(make_pair(str1[i], str1[i + 1]+32));
+		}
+		else if (check == 3)
+		{
+			temp1.push_back(make_pair(str1[i]+32, str1[i + 1]+32));
+		}
+	}
+
+	for (int i = 0; i < str2.size() - 1; ++i)
+	{
+		short check = 0;
+		for (int j = 0; j < 2; ++j)
+		{
+			if ('A' <= str2[i + j] && str2[i + j] <= 'Z')
+			{
+				if (j == 0)
+				{
+					check += 1;
+				}
+				else
+				{
+					check += 2;
+				}
+				continue;
+			}
+			else if ('a' <= str2[i + j] && str2[i + j] <= 'z')
+			{
+				continue;
+			}
+			else
+			{
+				check = -1;
+				break;
+			}
+		}
+		if (check == 0)
+		{
+			temp2.push_back(make_pair(str2[i], str2[i + 1]));
+		}
+		else if (check == 1)
+		{
+			temp2.push_back(make_pair(str2[i] + 32, str2[i + 1]));
+		}
+		else if (check == 2)
+		{
+			temp2.push_back(make_pair(str2[i], str2[i + 1] + 32));
+		}
+		else if (check == 3)
+		{
+			temp2.push_back(make_pair(str2[i] + 32, str2[i + 1] + 32));
+		}
+	}
+
+	for (int i = 0; i < temp1.size(); ++i)
+	{
+		int isExsist = -1;
+		for (int j = 0; j < count1.size(); ++j)
+		{
+			if (temp1[i].first == count1[j].second.first && temp1[i].second == count1[j].second.second)
+			{
+				isExsist = j;
+			}
+		}
+		if (isExsist != -1)
+		{
+			++count1[isExsist].first;
+		}
+		else
+		{
+			count1.push_back(make_pair(1, temp1[i]));
+		}
+	}
+	
+	for (int i = 0; i < temp2.size(); ++i)
+	{
+		int isExsist = -1;
+		for (int j = 0; j < count2.size(); ++j)
+		{
+			if (temp2[i].first == count2[j].second.first && temp2[i].second == count2[j].second.second)
+			{
+				isExsist = j;
+			}
+		}
+		if (isExsist != -1)
+		{
+			++count2[isExsist].first;
+		}
+		else
+		{
+			count2.push_back(make_pair(1, temp2[i]));
+		}
+	}
+
+	for (int i = 0; i < count1.size(); ++i)
+	{
+		for (int j = 0; j < count2.size(); ++j)
+		{
+			if (count1[i].second.first == count2[j].second.first && count1[i].second.second == count2[j].second.second)
+			{
+				interSection += min(count1[i].first, count2[j].first);
+			}
+		}
+	}
+
+	unionSection = temp1.size() + temp2.size() - interSection;
+
+	if (unionSection == 0) answer = 65536;
+	else answer = (int)((float)interSection / unionSection * 65536);
+
+	return answer;
+}
---
+++ WEEK27 ( 02-27 ~ 03~05 )/[1차] 캐시 - 김명진.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> caches;
+
+int main(int cacheSize, vector<string> cities)
+{
+    if (cacheSize == 0) return cities.size() * 5;
+
+    int answer = 0;
+
+    for (int i = 0; i < cities.size(); ++i)
+    {
+        for (int j = 0; j < cities[i].size(); ++j)
+        {
+            if ('A' <= cities[i][j] && cities[i][j] <= 'Z')
+            {
+                cities[i][j] += 32;
+            }
+        }
+    }
+
+    for (int i = 0; i < cities.size(); ++i)
+    {
+        int isHit = -1;
+        for (int j = 0; j < caches.size(); ++j)
+        {
+            if (cities[i] == caches[j])
+            {
+                isHit = j;
+            }
+        }
+
+        if (isHit != -1)
+        {
+            for (int k = isHit; k < caches.size()-1; ++k)
+            {
+                caches[k] = caches[k + 1];
+            }
+            caches[caches.size() - 1] = cities[i];
+            answer += 1;
+        }
+        else
+        {
+            if (caches.size() < cacheSize)
+            {
+                caches.push_back(cities[i]);
+            }
+            else
+            {
+                for (int k = 0; k < cacheSize - 1; ++k)
+                {
+                    caches[k] = caches[k + 1];
+                }
+                caches[cacheSize - 1] = cities[i];
+            }
+            answer += 5;
+        }
+    }
+
+    return answer;
+}
