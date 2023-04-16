59b98a608a5a8337b226209f5b30baff2ec8c293
thinkingjeen <111105992+thinkingjeen@users.noreply.github.com>
2023-04-16 18:59:57 +0900

    Create 외벽 점검 - 김명진.cpp

---
+++ WEEK32 ( 04-03 ~ 04-09 )/외벽 점검 - 김명진.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(int n, vector<int> weak, vector<int> dist) 
+{
+    int answer = 0;
+
+    vector<int> answerSheet;
+    int tempA = weak.size();
+    for (int i = 0; i < tempA-1; ++i)
+    {
+        weak.push_back(weak[i] + n);
+    }
+
+    sort(dist.begin(), dist.end());
+    do
+    {
+        for (int i = 0; i < tempA; ++i)
+        {
+            int tempB = 0;
+            for (int j = 0; j < dist.size(); ++j)
+            {
+                int cur = weak[i+tempB];
+                cur += dist[j];
+                while (cur >= weak[i + tempB] && i+tempB< weak.size()-1)
+                {
+                    ++tempB;
+                }
+                if (tempB >= tempA)
+                {
+                    answerSheet.push_back(++j);
+                }
+            }
+        }
+    } while (next_permutation(dist.begin(), dist.end()));
+
+    if (answerSheet.size() > 0)
+    {
+        sort(answerSheet.begin(), answerSheet.end());
+        answer = answerSheet[0];
+    }
+    else
+    {
+        return -1;
+    }
+    return answer;
+}
+
+int main()
+{
+}
